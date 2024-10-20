377d1f15ae8d5ed8413648ba46c4de2c913e09a6
Mohammad <malam2003@berkeley.edu>
2024-10-20 00:24:33 -0700

    checking rebuild

--- schoota/app/profile.tsx
+++ schoota/app/profile.tsx
@@ -1,7 +1,6 @@
 import { StyleSheet, Text, View } from 'react-native'
 import MapView from 'react-native-maps';
 import React from 'react'
-import { PROVIDER_GOOGLE } from 'react-native-maps';
 
 const Profile = () => {
   return (
